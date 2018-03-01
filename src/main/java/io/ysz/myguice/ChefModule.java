055ac4e63f716b68959465b1a0b3f1fb9c4a1cdd
Yakov Zaytsev <me@ysz.io>
2018-03-01 12:14:05 +0300

    subclass AbstractModule instead

--- src/main/java/io/ysz/myguice/ChefModule.java
+++ src/main/java/io/ysz/myguice/ChefModule.java
@@ -1,13 +1,12 @@
 package io.ysz.myguice;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Binder;
-import com.google.inject.Module;
 import com.google.inject.Scopes;
 
-public class ChefModule implements Module {
-    public void configure(Binder binder) {
-        binder.bind(FortuneService.class)
-              .to(FortuneServiceImpl.class)
-              .in(Scopes.SINGLETON);
+public class ChefModule extends AbstractModule {
+    protected void configure() {
+        bind(FortuneService.class)
+              .to(FortuneServiceImpl.class);
     }
 }
